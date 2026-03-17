5d8680b48c647f0890acb0ead21601373e230c25
thota murari <123160495+thotamurari@users.noreply.github.com>
2026-03-17 12:27:07 +0530

    Refactor PascalTriangle class and update logic

--- pascaltriangle.java
+++ pascaltriangle.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
-public class pascaltriangle {
+public class PascalTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=i; j++) {
-                System.out.print(i);
+
+        for (int i = 0; i < n; i++) {
+            int num = 1;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
             }
-                System.out.println();
+            System.out.println();
         }
     }
 }
