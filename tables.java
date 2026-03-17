5158ecefe71b6c0fb7852fb5a8c424b8452d452b
thota murari <123160495+thotamurari@users.noreply.github.com>
2026-03-17 12:28:57 +0530

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,11 @@
+public class tabel {
+    public static void main(String[] args) {
+        int n=10, tab=0, i;
+        for ( i=1; i<=n; i++ ) {
+            tab = n*i;
+            System.out.println(n+"x"+i+"="+ tab);
+
+        }
+    }
+    
+}
